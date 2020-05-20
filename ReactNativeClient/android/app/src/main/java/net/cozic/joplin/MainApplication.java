62c34b0e749f55304daf66a7ea8092e6658899aa
Roman Musin <musinrr@gmail.com>
2020-05-20 07:36:18 +0100

    WIP -- adds dynamic shortcuts which are shown on long press - not quite what i was hoping for

--- ReactNativeClient/android/app/src/main/java/net/cozic/joplin/MainApplication.java
+++ ReactNativeClient/android/app/src/main/java/net/cozic/joplin/MainApplication.java
@@ -6,6 +6,7 @@ import android.os.Build;
 import android.webkit.WebView;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.rnas.RNAppShortcutsPackage;
 import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
