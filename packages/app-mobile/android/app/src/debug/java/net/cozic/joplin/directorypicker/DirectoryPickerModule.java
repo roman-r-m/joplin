064f0f59be9cdbd5fc5d9466cc6c2ac6d89cc0e7
Roman <musinrr@gmail.com>
2021-03-21 22:04:12 +0000

    Show the actual file dir name - WIP

--- packages/app-mobile/android/app/src/debug/java/net/cozic/joplin/directorypicker/DirectoryPickerModule.java
+++ packages/app-mobile/android/app/src/debug/java/net/cozic/joplin/directorypicker/DirectoryPickerModule.java
@@ -1,20 +1,28 @@
 package net.cozic.joplin.directorypicker;
 
 import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Build;
+import android.provider.OpenableColumns;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
+import androidx.core.content.IntentCompat;
+import androidx.loader.content.CursorLoader;
 
 import com.facebook.react.bridge.ActivityEventListener;
+import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.WritableMap;
 
+import java.io.File;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class DirectoryPickerModule extends ReactContextBaseJavaModule implements ActivityEventListener {
@@ -43,10 +51,16 @@ public class DirectoryPickerModule extends ReactContextBaseJavaModule implements
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @ReactMethod
     public void pick(Promise promise) {
-        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
-        this.promise.set(promise);
-        if (!reactContext.startActivityForResult(intent, CODE, null)) {
-            promise.reject("1", "Failed to pick directory");
+        try {
+            Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
+            intent.putExtra("android.content.extra.SHOW_ADVANCED", true);
+            this.promise.set(promise);
+            if (!reactContext.startActivityForResult(intent, CODE, null)) {
+                promise.reject("1", "Failed to pick directory");
+                this.promise.set(null);
+            }
+        } catch (Exception e) {
+            promise.reject(e);
             this.promise.set(null);
         }
     }
@@ -61,11 +75,41 @@ public class DirectoryPickerModule extends ReactContextBaseJavaModule implements
             promise.reject("2", "Cancelled");
             return;
         }
+        WritableMap map = Arguments.createMap();
+
         Uri uri = data.getData();
-        promise.resolve(uri.toString());
+        map.putString("uri", uri.toString());
+        map.putString("patht", getFileName(uri, activity.getContentResolver()));
+        promise.resolve(map);
     }
 
     @Override
     public void onNewIntent(Intent intent) {
+    }
+
+    private String getFileName(Uri uri, ContentResolver contentResolver) {
+        if (ContentResolver.SCHEME_FILE.equals(uri.getScheme())) {
+            File file = new File(uri.getPath());
+            return file.getName();
+        } else if (ContentResolver.SCHEME_CONTENT.equals(uri.getScheme())) {
+            String name = null;
+            CursorLoader cursorLoader = new CursorLoader(reactContext, uri, null, null, null, null);
+            Cursor cursor = cursorLoader.loadInBackground();
+//            Cursor cursor = contentResolver.query(uri, null, null, null, null);
+            if (cursor != null) {
+                try {
+                    if (cursor.moveToFirst()) {
+                        int nameIndex = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
+                        name = cursor.getString(nameIndex);
+                    }
+                } finally {
+                    cursor.close();
+                }
+            }
+            return name;
+        } else {
+            Log.w("joplin", "Unknown URI scheme: " + uri.getScheme());
+            return null;
+        }
     }
 }
