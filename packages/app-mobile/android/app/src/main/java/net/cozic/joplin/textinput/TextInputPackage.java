33897fb4e8636aa75b34d56e998c50c9e11ba85a
Roman <musinrr@gmail.com>
2021-05-24 20:05:29 +0100

    WIP with markwon

--- packages/app-mobile/android/app/src/main/java/net/cozic/joplin/textinput/TextInputPackage.java
+++ packages/app-mobile/android/app/src/main/java/net/cozic/joplin/textinput/TextInputPackage.java
@@ -1,14 +1,10 @@
 package net.cozic.joplin.textinput;
 
-import android.graphics.Color;
+import android.graphics.Typeface;
 import android.text.Editable;
-import android.text.InputType;
 import android.text.Selection;
-import android.text.Spannable;
 import android.text.Spanned;
-import android.text.TextWatcher;
-import android.text.style.ForegroundColorSpan;
-import android.util.Log;
+import android.text.style.StyleSpan;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -16,16 +12,21 @@ import androidx.annotation.Nullable;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReadableArray;
-import com.facebook.react.uimanager.ThemedReactContext;
-import com.facebook.react.uimanager.ViewDefaults;
 import com.facebook.react.uimanager.ViewManager;
-import com.facebook.react.uimanager.ViewProps;
 import com.facebook.react.uimanager.annotations.ReactProp;
 import com.facebook.react.views.textinput.ReactEditText;
 import com.facebook.react.views.textinput.ReactTextInputManager;
 
 import java.util.Collections;
 import java.util.List;
+
+import io.noties.markwon.Markwon;
+import io.noties.markwon.core.spans.StrongEmphasisSpan;
+import io.noties.markwon.editor.AbstractEditHandler;
+import io.noties.markwon.editor.MarkwonEditor;
+import io.noties.markwon.editor.MarkwonEditorTextWatcher;
+import io.noties.markwon.editor.MarkwonEditorUtils;
+import io.noties.markwon.editor.PersistedSpans;
 
 public class TextInputPackage implements com.facebook.react.ReactPackage {
     @NonNull
@@ -50,27 +51,86 @@ public class TextInputPackage implements com.facebook.react.ReactPackage {
 
             @ReactProp(name = "enableMdHighlight")
             public void setEnableMdHighlight(ReactEditText view, boolean value) {
-                Log.i("ZZZ", "setEnableMdHighlight");
                 if (value) {
-                    view.addTextChangedListener(new TextWatcher() {
-                        @Override
-                        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+//                    ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+                    final Markwon markwon = Markwon.builder(reactContext)
+                            .build();
+                    final MarkwonEditor editor = MarkwonEditor.builder(markwon)
+//                            .useEditHandler(new AbstractEditHandler<StrongEmphasisSpan>() {
+//                                @Override
+//                                public void configurePersistedSpans(@NonNull PersistedSpans.Builder builder) {
+//                                    // Here we define which span is _persisted_ in EditText, it is not removed
+//                                    //  from EditText between text changes, but instead - reused (by changing
+//                                    //  position). Consider it as a cache for spans. We could use `StrongEmphasisSpan`
+//                                    //  here also, but I chose Bold to indicate that this span is not the same
+//                                    //  as in off-screen rendered markdown
+//                                    builder.persistSpan(Bold.class, Bold::new);
+//                                }
+//
+//                                @Override
+//                                public void handleMarkdownSpan(
+//                                        @NonNull PersistedSpans persistedSpans,
+//                                        @NonNull Editable editable,
+//                                        @NonNull String input,
+//                                        @NonNull StrongEmphasisSpan span,
+//                                        int spanStart,
+//                                        int spanTextLength) {
+//                                    // Unfortunately we cannot hardcode delimiters length here (aka spanTextLength + 4)
+//                                    //  because multiple inline markdown nodes can refer to the same text.
+//                                    //  For example, `**_~~hey~~_**` - we will receive `**_~~` in this method,
+//                                    //  and thus will have to manually find actual position in raw user input
+//                                    final MarkwonEditorUtils.Match match =
+//                                            MarkwonEditorUtils.findDelimited(input, spanStart, "**", "__");
+//                                    if (match != null) {
+//                                        editable.setSpan(
+//                                                // we handle StrongEmphasisSpan and represent it with Bold in EditText
+//                                                //  we still could use StrongEmphasisSpan, but it must be accessed
+//                                                //  via persistedSpans
+//                                                persistedSpans.get(Bold.class),
+//                                                match.start(),
+//                                                match.end(),
+//                                                Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+//                                        );
+//                                    }
+//                                }
+//
+//                                @NonNull
+//                                @Override
+//                                public Class<StrongEmphasisSpan> markdownSpanType() {
+//                                    return StrongEmphasisSpan.class;
+//                                }
+//                            })
+                            .build();
+                    view.addTextChangedListener(MarkwonEditorTextWatcher.withProcess(editor));
 
-                        }
-
-                        @Override
-                        public void onTextChanged(CharSequence s, int start, int before, int count) {
-                            if (before > 0) {
-                                ((Spannable) s).setSpan(new ForegroundColorSpan(Color.GRAY), start, start + count,
-                                        Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-                            }
-                        }
-
-                        @Override
-                        public void afterTextChanged(Editable s) {
-
-                        }
-                    });
+//                    view.addTextChangedListener(new TextWatcher() {
+//
+//                        private ScheduledFuture<?> future;
+//
+//                        @Override
+//                        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+//
+//                        }
+//
+//                        @Override
+//                        public void onTextChanged(CharSequence s, int start, int before, int count) {
+////                            if (count < s.length()) {
+//////                                ((Spannable) s).setSpan(new ForegroundColorSpan(Color.GRAY), start, start + count,
+//////                                        Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+//////                                node = parser.parse(s.toString());
+////                            }
+//                        }
+//
+//                        @Override
+//                        public void afterTextChanged(Editable s) {
+//                            if (future != null) {
+//                                future.cancel(true);
+//                            }
+//                            future = executor.schedule(() -> UiThreadUtil.runOnUiThread(() -> {
+//                                markwon.setMarkdown(view, s.toString());
+//                            }), 100, TimeUnit.MILLISECONDS);
+//                        }
+//                    });
                 }
             }
 
@@ -84,5 +144,12 @@ public class TextInputPackage implements com.facebook.react.ReactPackage {
                 super.receiveCommand(reactEditText, commandId, args);
             }
         });
+    }
+
+    private static final class Bold extends StyleSpan {
+
+        public Bold() {
+            super(Typeface.BOLD);
+        }
     }
 }
